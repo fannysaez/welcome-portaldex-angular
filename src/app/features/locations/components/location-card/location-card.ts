d4a5d778433b89616d69bbf5119ebf901e13dcfb
Fanny Saez <saez.fanny.63@gmail.com>
2026-07-20 15:31:57 +0200

    chore: scaffold folder structure for episodes and locations features

---
+++ src/app/features/locations/components/location-card/location-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-card',
+  imports: [],
+  templateUrl: './location-card.html',
+  styleUrl: './location-card.css',
+})
+export class LocationCard {
+
+}
